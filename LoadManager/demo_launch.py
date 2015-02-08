09a744f6f0eadde662e2df4c70f9bb7aef18939a
asdf <asdf@asdf.(none)>
2015-02-07 20:38:44 -0500

    demo file

--- LoadManager/demo_launch.py
+++ LoadManager/demo_launch.py
@@ -3,7 +3,13 @@ Launch file to start all process for thesis demo.
 """
 
 import spur
+import time
 
 shell = spur.LocalShell()
-result = shell.run(["echo", "-n", "hello"])
-print result.output
+minimum_launch = shell.spawn(["roslaunch", "turtlebot_bringup", "minimal.launch"], 
+                             stdout=1, store_pid=True)
+
+print minimum_launch.pid
+minimum_launch.wait_for_result()
+    
+
