c6c8548a42fe94814f1cdb09a849416815507ec6
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-02-12 16:34:28 -0500

    edited demo_launch

--- LoadManager/demo_launch.py
+++ LoadManager/demo_launch.py
@@ -2,16 +2,26 @@
 Launch file to start all process for thesis demo.
 """
 
-import subprocess
 import spur
-import time, sys
+import time, sys, os
 
 start = time.time()
-shell = spur.LocalShell()
-command = ["python", "test_stdout.py"]
-process = shell.spawn(command, stdout=sys.stdout, store_pid=True)
+
+# set up shell
+shell = spur.SshShell(hostname="10.8.244.74", 
+                      username="pi", 
+                      password="raspberry") 
+command = ["python", "-u", "test_stdout.py"]
+path = "/home/pi/thesis/LoadManager"
+
+# launch process
+process = shell.spawn(command,
+                      cwd=path,
+                      stdout=sys.stdout, 
+                      store_pid=True)
 
 print process.pid
+#print process.output
 message = "[demo_launch] Waiting... Elapsed time: "
 while True:
     time.sleep(5)
