d60a0b1e198a4360b8dd5abb1857f3e16f70ccd2
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-02-17 00:26:27 -0500

    basic demo launcher works

--- LoadManager/demo_subprocess.py
+++ LoadManager/demo_subprocess.py
@@ -9,26 +9,76 @@ sets up processes on each of them by sending commands over stdin.
 import subprocess
 import time, sys, os
 
-# set up shells
-RPI = "pi@10.8.244.74"
-RPI_PASSWORD = "raspberry"
 
-SQUIRREL = "squirrel@squirrel"
-SQUIRREL_PASSWORD = "asdf"
+def openSSH(ip, usr):
+   
+    SSH = ["ssh", "-t", "-t", usr + "@" + ip]
+    ssh = subprocess.Popen(SSH, stdin=subprocess.PIPE)
+    return ssh
 
-SSH = ["ssh", "-t", "-t", SQUIRREL]
-COMMAND = "printenv"
+def navigation():
+    # set up shells
+    RPI = "pi@10.8.244.74"
+    SQUIRREL = "squirrel"
+    ASDF = "asdf"
 
-ssh = subprocess.Popen(SSH, 
-                       stdin=subprocess.PIPE, 
-#                       stdout=subprocess.PIPE, 
-                       stderr=subprocess.PIPE)
+    ROSCORE = "roscore"
+    MIN_LAUNCH = "roslaunch turtlebot_bringup minimal.launch"
+    SENSE_LAUNCH = "roslaunch turtlebot_bringup 3dsensor_edited.launch"
+    AMCL_LAUNCH = "roslaunch turtlebot_navigation amcl_demo_edited.launch map_file:=/tmp/ee_lab_big.yaml"
 
-#ssh.stdin.write(SQUIRREL_PASSWORD + "\n")
-#ssh.stdin.flush()
-ssh.stdin.write(COMMAND + "\n")
+    roscore = openSSH(SQUIRREL, SQUIRREL)
+    roscore.stdin.write(ROSCORE + "\n")
+    time.sleep(5)
+    
+    minlaunch = openSSH(ASDF, ASDF)
+    minlaunch.stdin.write(MIN_LAUNCH + "\n")
+    time.sleep(5)
+    
+    senselaunch = openSSH(ASDF, ASDF)
+    senselaunch.stdin.write(SENSE_LAUNCH + "\n")
+    time.sleep(5)
 
-print "holding loop"
-while True:
-    time.sleep(1)
+    amcllaunch = openSSH(SQUIRREL, SQUIRREL)
+    amcllaunch.stdin.write(AMCL_LAUNCH + "\n")
 
+def mapping():
+    # set up shells
+    RPI = "pi@10.8.244.74"
+    SQUIRREL = "squirrel"
+    ASDF = "asdf"
+
+    ROSCORE = "roscore"
+    MIN_LAUNCH = "roslaunch turtlebot_bringup minimal.launch"
+    MAP_LAUNCH = "roslaunch turtlebot_navigation gmapping_demo.launch"
+
+    # "roslaunch turtlebot_rviz_launchers view_navigation.launch"
+
+    roscore = openSSH(SQUIRREL, SQUIRREL)
+    roscore.stdin.write(ROSCORE + "\n")
+    time.sleep(5)
+    
+    minlaunch = openSSH(ASDF, ASDF)
+    minlaunch.stdin.write(MIN_LAUNCH + "\n")
+    time.sleep(5)
+    
+    maplaunch = openSSH(ASDF, ASDF)
+    maplaunch.stdin.write(MAP_LAUNCH + "\n")
+
+def main():
+
+    KILL = "kill -15 "
+    
+
+    try:
+#        mapping()
+        navigation()
+
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
