9e5b0adaa0b388d56f3f5de0763dc97436800b7b
asdf <asdf@asdf.(none)>
2015-03-13 19:27:10 -0400

    added setPosition, changed testXML so it takes command line arguments

---
+++ LoadManager/catkin_ws/src/load_manager/src/setPosition.py
@@ -0,0 +1,20 @@
+import sys
+from lxml import etree
+
+PATH = "/opt/ros/hydro/share/turtlebot_navigation/launch/"
+FILE = "amcl_demo_edited.launch"
+
+tree = etree.parse(PATH + FILE)
+root = tree.getroot()
+
+for elem in root.findall("arg"):
+    if elem.attrib["name"] == "initial_pose_x":
+        elem.attrib["default"] = sys.argv[2]
+    if elem.attrib["name"] == "initial_pose_y":
+        elem.attrib["default"] = sys.argv[3]
+    if elem.attrib["name"] == "initial_pose_a":
+        elem.attrib["default"] = sys.argv[4]
+
+        
+with open(PATH + FILE, 'w') as file_handle:
+    file_handle.write(etree.tostring(tree, pretty_print=True, encoding='utf8'))
