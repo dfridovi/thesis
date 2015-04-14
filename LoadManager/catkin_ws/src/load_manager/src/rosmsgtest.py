711050aa97700b01c1b77b8d847cc26430f7ebf0
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-04-14 16:57:51 -0400

    made a cool plot

--- LoadManager/catkin_ws/src/load_manager/src/rosmsgtest.py
+++ LoadManager/catkin_ws/src/load_manager/src/rosmsgtest.py
@@ -1,15 +1,32 @@
 import move_base_msgs.msg
 import geometry_msgs.msg
 import nav_msgs.msg
+import sensor_msgs.msg as sm
 import rospy
 
+filtered = 0
+
 def move_base_subscriber_callback(data):
-	print data.pose
-	
+	global filtered
+	now = rospy.get_rostime()
+	#print now.nsecs
+	#print now.secs
+	#print data.header.stamp.nsecs
+	#print data.header.stamp.secs
+	#print now.nsecs-data.header.stamp.nsecs
+	#print now.secs-data.header.stamp.secs
+	#print (now.nsecs-data.header.stamp.nsecs)/10e9
+	#print (now.nsecs-data.header.stamp.nsecs)/10e9 + (now.secs-data.header.stamp.secs)
+	msecs = ((now.nsecs-data.header.stamp.nsecs)/10e9 + (now.secs-data.header.stamp.secs))*1000
+	filtered = .99*filtered + .01 * msecs
+	print "unfiltered: " + str(msecs) + "filtered: " + str(filtered)
 
 if __name__ == "__main__":
 	rospy.init_node("listener",anonymous = True)
 	callback = move_base_subscriber_callback
 	#rospy.Subscriber("/move_base/feedback", move_base_msgs.msg.MoveBaseActionFeedback,callback)
-	rospy.Subscriber("/odom", nav_msgs.msg.Odometry,callback)
+	#rospy.Subscriber("/odom", nav_msgs.msg.Odometry,callback)
+	#rospy.Subscriber("/camera/depth/image_raw", sm.Image,callback)
+	#rospy.Subscriber("/scan", sm.LaserScan, callback)
+	rospy.Subscriber("/camera/depth/image_raw/compressed", sm.CompressedImage, callback)
 	rospy.spin()
--- LoadManager/data/plot_data.py
+++ LoadManager/data/plot_data.py
@@ -18,16 +18,18 @@ for machine, row in zip(data.keys(), range(1, num_machines + 1)):
     # plot the cpu utilization data
     plt.subplot(num_machines, 1, row)
     time = data[machine]["time"]
-    activity = data[machine]["activity"]
+    filtered_activity = data[machine]["filtered activity"]
+    raw_activity = data[machine]["raw activity"]
 
-    plt.plot(time, activity)
+    plt.plot(time, raw_activity, 'go', markersize=1)
+    plt.plot(time, filtered_activity, 'b--')
     plt.title("Activity for Machine: %s" % machine)
     plt.xlabel("Time since program initialization (seconds)")
     plt.ylabel("CPU utilization percentage")
 
     # insert vertical lines for each process start
-    for process in data[machine]["processes"].keys():
-        stamp = data[machine]["processes"][process]
-        plt.axvline(stamp, color="r")
+#    for process in data[machine]["processes"].keys():
+#        stamp = data[machine]["processes"][process]
+#        plt.axvline(stamp, color="r")
 
 plt.show()
