103744ce735bec41685517d70bb2b4d015087833
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-04-20 17:12:11 -0400

    more data

--- LoadManager/catkin_ws/src/load_manager/src/rosmsgtest.py
+++ LoadManager/catkin_ws/src/load_manager/src/rosmsgtest.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
 		callback = move_base_subscriber_callback
 		#rospy.Subscriber("/move_base/feedback", move_base_msgs.msg.MoveBaseActionFeedback,callback)
 		#rospy.Subscriber("/odom", nav_msgs.msg.Odometry,callback)
-		#rospy.Subscriber("/camera/depth/image_raw", sm.Image,callback)
+		rospy.Subscriber("/camera/depth/image_raw", sm.Image,callback)
 		#rospy.Subscriber("/scan", sm.LaserScan, callback)
-		rospy.Subscriber("/camera/depth/image_raw/compressed", sm.CompressedImage, callback)
+		#rospy.Subscriber("/camera/depth/image_raw/compressed", sm.CompressedImage, callback)
 		while True:
 			pass
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 		import cPickle as pickle
 		import numpy as np
 		global msecsdata		
-		file = open("msecsdata.pkl", 'wb')
+		file = open("rawdepthmsecsdata.pkl", 'wb')
 		pickle.dump(np.asarray(msecsdata, dtype=np.float), file)
 		file.close()
 		sys.exit(0)
