712f9e0fdf538294cc27ab26812c0eb2d1e956c7
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-08-26 21:18:58 -0700

    plot comparison of histogram times

--- PingStatistics/plot_hist.py
+++ PingStatistics/plot_hist.py
@@ -5,16 +5,25 @@ import numpy as np
 
 # plots histogram from pkl file
 
-file = open(sys.argv[1], 'rb')
-data = pickle.load(file)
-file.close()
+file1 = open(sys.argv[1], 'rb')
+file2 = open(sys.argv[2], 'rb')
 
-thresh = np.percentile(data, 100.0)
-print thresh
+data1 = pickle.load(file1)
+data2 = pickle.load(file2)
 
-n, bins, patches = plt.hist(data[data <= thresh], 20, facecolor='g', alpha=0.75)
-plt.xlabel("Message Age Distribution (ms)")
-plt.ylabel('Frequency')
-plt.title('Message Age of /scan')
+file1.close()
+file2.close()
+
+thresh1 = np.percentile(data1, 95.0)
+thresh2 = np.percentile(data2, 95.0)
+print thresh1, thresh2
+
+n, bins, patches = plt.hist(data1[data1 <= thresh1], 20, facecolor='g', alpha=0.75)
+n, bins, patches = plt.hist(data2[data2 <= thresh2], 20, facecolor='r', alpha=0.75)
+plt.tick_params(labelsize=14)
+plt.xlabel("Ping Times (ms)", fontsize=16)
+plt.ylabel("Normalized Frequency", fontsize=16)
+plt.title("Comparison of Ping Times", fontsize=18)
+plt.legend(["Local Network", "Google"])
 plt.grid(True)
 plt.show()
